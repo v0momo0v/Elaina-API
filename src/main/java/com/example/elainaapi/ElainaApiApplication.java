075fb5793587813eb69267073abb9c2be494b617
Gavin <1524018075@qq.com>
2023-12-10 17:15:41 +0800

    API项目

---
+++ src/main/java/com/example/elainaapi/ElainaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.elainaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElainaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElainaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elainaapi/ElainaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elainaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElainaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
